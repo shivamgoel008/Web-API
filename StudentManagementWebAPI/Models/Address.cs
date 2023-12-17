9538178038b7b26a85440fef92c68beb7d669a52
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2023-12-17 16:26:56 +0530

    Db scaffold done, coonection established

---
+++ StudentManagementWebAPI/Models/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentManagementWebAPI.Models
+{
+    public partial class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddress { get; set; } = null!;
+        public string PostalAddress { get; set; } = null!;
+        public Guid StudentId { get; set; }
+
+        public virtual Student Student { get; set; } = null!;
+    }
+}
---
+++ StudentManagementWebAPI/Models/Gender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentManagementWebAPI.Models
+{
+    public partial class Gender
+    {
+        public Gender()
+        {
+            Students = new HashSet<Student>();
+        }
+
+        public Guid Id { get; set; }
+        public string Description { get; set; } = null!;
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
