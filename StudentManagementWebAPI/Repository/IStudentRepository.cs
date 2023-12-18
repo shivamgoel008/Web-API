14633c378be3ca2498f8861b03745d51e8269f42
Shivam Kumar <55030452+shivamgoel008@users.noreply.github.com>
2023-12-18 22:28:07 +0530

    add student post request done, model error resolved, fk exception resolved

--- StudentManagementWebAPI/Repository/IStudentRepository.cs
+++ StudentManagementWebAPI/Repository/IStudentRepository.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using StudentManagementWebAPI.Models;
+using StudentManagementWebAPI.ServiceLayerModels;
 
 namespace StudentManagementWebAPI.Repository
 {
     public interface IStudentRepository
     {
         Task<List<Student>> GetAllStudentsAsync();
+        Task<Student> AddStudent(StudentSL student);
     }
 }
 
--- StudentManagementWebAPI/Repository/StudentRepository.cs
+++ StudentManagementWebAPI/Repository/StudentRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using StudentManagementWebAPI.Models;
+using StudentManagementWebAPI.ServiceLayerModels;
 
 namespace StudentManagementWebAPI.Repository
 {
@@ -11,6 +13,25 @@ namespace StudentManagementWebAPI.Repository
         {
             context = _studentDBContext;
         }
+
+        public async Task<Student> AddStudent(StudentSL student)
+        {
+            var genderId = await context.Genders.Where(x => x.Description == student.Gender).Select(x => x.Id).FirstOrDefaultAsync();
+            Student studentDto = new Student
+            {
+                FirstName = student.FirstName,
+                LastName = student.LastName,
+                DateOfBirth = student.DateOfBirth,
+                Email = student.Email,
+                Mobile = student.Mobile,
+                ProfileImageUrl = student.ProfileImageUrl,
+                GenderId = genderId,
+            };
+            this.context.Students.Add(studentDto);
+            context.SaveChanges();
+            return studentDto;
+        }
+
         public async Task<List<Student>> GetAllStudentsAsync()
         {
             List<Student> students = await context.Students.ToListAsync();
